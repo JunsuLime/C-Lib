f58d81b7b03aa7cfdfb385298a32ca2d22b9ee98
junsulime <vfv0121@gmail.com>
2018-05-02 21:17:09 +0900

    [Work] Add MIT License to file's header

--- include/arraylist.h
+++ include/arraylist.h
@@ -1,3 +1,39 @@
+/**
+ * C collection ArrayList: arraylist.h
+ *
+ * MIT License
+ *
+ * Copyright (c) 2018 Junsulime
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+// TODO: Add comment on functions
+//
+// - What the inputs and outputs are.
+// - For class member functions: whether the object remembers reference arguments
+//   beyond the duration of the method call, and whether it will free them or not.
+// - If the function allocates memory that the caller must free.
+// - Whether any of the arguments can be a null pointer.
+// - If there are any performance implications of how a function is used.
+
+
 #ifndef ARRAYLIST_H
 #define ARRAYLIST_H
 
@@ -6,8 +42,11 @@
 #define INIT_ARRAY_SIZE 16
 
 typedef struct {
+	// Has item array's pointer
 	int *_container;
+	// Has item array's physical size
 	int _size;
+	// Has item array's logical size
 	int length;
 } ArrayList;
 
@@ -73,4 +112,4 @@ void arrayListClear(ArrayList  *arrayList);
 
 int arrayListLength(ArrayList *arrayList);
 
-#endif
+#endif	// ARRAYLIST_H
